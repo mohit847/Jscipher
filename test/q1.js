9561ffd00e92dda51df9bccaf044299d7f01cbd6
Mohit Kumar <mohitkvk455@gmail.com>
2023-06-17 19:27:14 +0530

    test

---
+++ test/q1.js
@@ -0,0 +1,13 @@
+// Question 1:
+// Write a JavaScript function that takes two numbers as input and returns their sum.
+// Input: Two numbers, e.g., 5 and 10.
+// Output: The sum of the two numbers, e.g., 15.
+
+
+
+function twoSum(num1, num2){
+    let result = num1 + num2;
+    console.log(result)
+}
+
+twoSum(4,5)
---
+++ test/q2.js
@@ -0,0 +1,4 @@
+// Question 2:
+// Write a JavaScript function that takes a string as input and returns the length of that string.
+// Input: A string, e.g., "Hello, world!".
+// Output: The length of the string, e.g., 13.
